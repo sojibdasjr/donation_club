74479ffd0c24cddc1be1d7327b9fd1ab74a6768f
sojibdasjr <contact.sojibdas@gmail.com>
2024-03-06 21:32:30 +0600

    creact donation application and install router-dom tailwind daisy ui and setup route

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root";
+import Home from "./components/Pages/Home";
+import Contact from "./components/Pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
